729dee0835f47fbd93fb0da5712189039e01f749
Rohama Majeed <rohamamajeed3@gmail.com>
2026-07-17 09:23:48 +0300

    footer set

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="bg-[#010E1B] w-full text-white pb-4 border-t border-[#55B4F3]/20">
+    <footer className="bg-[#010E1B] w-full text-white flex flex-col pb-4 border-t border-[#55B4F3]/20">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-3 md:py-4 flex flex-col sm:flex-row justify-between items-center gap-3">
         {/* Logo / Title */}
         <img src="src/assets/logo.png" alt="MLSA UET Taxila" className="w-20 h-20  " />
